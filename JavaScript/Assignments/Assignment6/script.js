b2c3c96095adf4a2ee651fcc1c77763f8d40081c
Asad Ali <asadali27232@gmail.com>
2022-12-02 12:50:27 +0500

    Updated

--- JavaScript/Assignments/Assignment6/script.js
+++ JavaScript/Assignments/Assignment6/script.js
@@ -26,14 +26,63 @@ function playing(event) {
         box.style.backgroundImage = 'url(images/circle-o.png)'
         player1 = false
         player2 = true
+        fillArray('O', event.target.id)
+        win('O')
     }
     else {
         box.style.backgroundImage = 'url(images/cross-x.png)'
         player1 = true
         player2 = false
+        fillArray('X', event.target.id)
+        win('X')
     }
 
     box.removeEventListener('click', playing)
-    console.log(arr)
 
+}
+function fillArray(symbol, pos) {
+    if (pos == 'OX1')
+        arr[0][0] = symbol
+    else if (pos == 'OX2')
+        arr[0][1] = symbol
+    else if (pos == 'OX3')
+        arr[0][2] = symbol
+    else if (pos == 'OX4')
+        arr[1][0] = symbol
+    else if (pos == 'OX5')
+        arr[1][1] = symbol
+    else if (pos == 'OX6')
+        arr[1][2] = symbol
+    else if (pos == 'OX7')
+        arr[2][0] = symbol
+    else if (pos == 'OX8')
+        arr[2][1] = symbol
+    else if (pos == 'OX9')
+        arr[2][2] = symbol
+}
+function win(player) {
+    let win = false
+    console.log(win)
+
+    if (arr[0][0] == arr[0][1] && arr[0][1] == arr[0][2])
+        win = true
+    else if (arr[1][0] == arr[1][1] && arr[1][1] == arr[1][2])
+        win = true
+    else if (arr[2][0] == arr[2][1] && arr[2][1] == arr[2][2])
+        win = true
+    else if (arr[0][0] == arr[1][0] && arr[1][0] == arr[2][0])
+        win = true
+    else if (arr[1][1] == arr[1][1] && arr[1][1] == arr[2][1])
+        win = true
+    else if (arr[0][2] == arr[1][2] && arr[1][2] == arr[2][2])
+        win = true
+    else if (arr[0][0] == arr[1][1] && arr[1][1] == arr[2][2])
+        win = true
+    else if (arr[0][2] == arr[1][1] && arr[1][1] == arr[2][0])
+        win = true
+
+    console.log(win)
+    if (win == true) {
+        console.log("Player " + player + " Wins...!")
+    }
 }
