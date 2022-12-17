78561a8a811d1180e49a394bcda3632e87ae116c
Asad Ali <asadali27232@gmail.com>
2022-12-17 18:03:44 +0500

    Updated

--- JavaScript/Assignments/Assignment8/script.js
+++ JavaScript/Assignments/Assignment8/script.js
@@ -1,4 +1,7 @@
 let formSectionHidden = false
+let infoSectionHidden = false
+let expSectionHidden = false
+
 document.getElementById("form-section-opener").addEventListener('click', function () {
     const formSection = document.getElementById('form-section')
     if (formSectionHidden) {
@@ -9,5 +12,30 @@ document.getElementById("form-section-opener").addEventListener('click', functio
         formSection.style.display = 'none'
         formSection.style.visibility = 'hidden'
         formSectionHidden = true
+    }
+})
+
+document.getElementById("info-section-opener").addEventListener('click', function () {
+    const infoSection = document.getElementById('info-section')
+    if (infoSectionHidden) {
+        infoSection.style.display = 'flex'
+        infoSection.style.visibility = 'visible'
+        infoSectionHidden = false
+    } else {
+        infoSection.style.display = 'none'
+        infoSection.style.visibility = 'hidden'
+        infoSectionHidden = true
+    }
+})
+document.getElementById("experience-section-opener").addEventListener('click', function () {
+    const expSection = document.getElementById('experience-section')
+    if (expSectionHidden) {
+        expSection.style.display = 'flex'
+        expSection.style.visibility = 'visible'
+        expSectionHidden = false
+    } else {
+        expSection.style.display = 'none'
+        expSection.style.visibility = 'hidden'
+        expSectionHidden = true
     }
 })
