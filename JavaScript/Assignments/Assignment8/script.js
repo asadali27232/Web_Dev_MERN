ac636b5eaec8c8ecf76a2087ae5d6d318eb00aec
Asad Ali <asadali27232@gmail.com>
2022-12-17 20:40:47 +0500

    Updated

--- JavaScript/Assignments/Assignment8/script.js
+++ JavaScript/Assignments/Assignment8/script.js
@@ -1,6 +1,9 @@
-let formSectionHidden = false
+let formSectionHidden = true
 let infoSectionHidden = false
-let expSectionHidden = false
+let expSectionHidden = true
+let eduSectionHidden = true
+let skillSectionHidden = true
+let langSectionHidden = true
 
 document.getElementById("form-section-opener").addEventListener('click', function () {
     const formSection = document.getElementById('form-section')
@@ -37,5 +40,41 @@ document.getElementById("experience-section-opener").addEventListener('click', f
         expSection.style.display = 'none'
         expSection.style.visibility = 'hidden'
         expSectionHidden = true
+    }
+})
+document.getElementById("education-section-opener").addEventListener('click', function () {
+    const eduSection = document.getElementById('education-section')
+    if (eduSectionHidden) {
+        eduSection.style.display = 'flex'
+        eduSection.style.visibility = 'visible'
+        eduSectionHidden = false
+    } else {
+        eduSection.style.display = 'none'
+        eduSection.style.visibility = 'hidden'
+        eduSectionHidden = true
+    }
+})
+document.getElementById("skill-section-opener").addEventListener('click', function () {
+    const skillSection = document.getElementById('skill-section')
+    if (skillSectionHidden) {
+        skillSection.style.display = 'flex'
+        skillSection.style.visibility = 'visible'
+        skillSectionHidden = false
+    } else {
+        skillSection.style.display = 'none'
+        skillSection.style.visibility = 'hidden'
+        skillSectionHidden = true
+    }
+})
+document.getElementById("language-section-opener").addEventListener('click', function () {
+    const languageSection = document.getElementById('language-section')
+    if (langSectionHidden) {
+        languageSection.style.display = 'flex'
+        languageSection.style.visibility = 'visible'
+        langSectionHidden = false
+    } else {
+        languageSection.style.display = 'none'
+        languageSection.style.visibility = 'hidden'
+        langSectionHidden = true
     }
 })
