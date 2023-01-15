e1f409e799223feeb2697c58af49abd4bda97782
Asad Ali <asadali27232@gmail.com>
2023-01-16 00:23:48 +0500

    Updated

--- React_JS/Assignments/assignment01/src/components/HomeBottomSec.js
+++ React_JS/Assignments/assignment01/src/components/HomeBottomSec.js
@@ -1,28 +1,34 @@
 import React from 'react'
 import './HomeBottomSec.css'
+import { Link } from 'react-router-dom'
 
 function HomeBottomSec() {
     return (
         <>
-            <div className="banner">
-                <div className="banner_image">
-                    <h1>STYLE GUIDE</h1>
-                    <p>TIPS FROM OUR STYLIST</p>
-                </div>
-                <div className="banner_form">
-                    <h3>GET 5% OFF!</h3>
-                    <h4>SIGN UP NOW TO OUR NEWSLETTER</h4>
+            <div className="col-12">
+                <div className="row m-0 py-20">
+                    <div className="col-md-6 m-0 p-0 h-max banner_image">
+                        <Link className=' m-0 p-0' to="/">
+                            <img src="images/home-bottom-banner.jpg" className='w-full' alt="BOTTOM BANNER" />
+                        </Link>
+                    </div>
+                    <div className="col-md-6 banner_form_section">
+                        <div className='banner_form h-44 my-16 mx-10'>
+                            <h3>GET 5% OFF!</h3>
+                            <h4>SIGN UP NOW TO OUR NEWSLETTER</h4>
 
-                    <div className="input">
-                        <input
-                            type="text"
-                            name="gift_code"
-                            id="gift_code"
-                            className="field"
-                        />
-                        <button className="sign-up_btn" type="submit">
-                            SIGN ME UP
-                        </button>
+                            <div className="sign-up-input w-full">
+                                <input
+                                    type="text"
+                                    name="gift_code"
+                                    id="gift_code"
+                                    className="field px-2"
+                                />
+                                <button className="sign-up_btn" type="submit">
+                                    SIGN ME UP
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
