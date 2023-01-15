be3519879667320b241a3f04eeb47d49d3268f33
Asad Ali <asadali27232@gmail.com>
2023-01-15 13:33:41 +0500

    Updated

--- React_JS/Assignments/assignment01/src/App.js
+++ React_JS/Assignments/assignment01/src/App.js
@@ -1,9 +1,39 @@
 import './App.css';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import ProductDetail from './pages/ProductDetail';
+import Cart from './pages/Cart';
+import Checkout from './pages/Checkout'
+import Login from './pages/Login';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <>
-      
+      <BrowserRouter>
+        <div>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/products">Products</Link></li>
+            <li><Link to="/product">ProductDetail</Link></li>
+            <li><Link to="/login">Login</Link></li>
+          </ul>
+          <hr />
+        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/product" element={<ProductDetail />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
