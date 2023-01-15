5e3491110d5f06e463796c7efa53408852dc241c
Asad Ali <asadali27232@gmail.com>
2023-01-16 03:27:29 +0500

    Updated

--- React_JS/Assignments/assignment01/src/components/Header.js
+++ React_JS/Assignments/assignment01/src/components/Header.js
@@ -4,10 +4,9 @@ import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import { Link } from 'react-router-dom';
-
+import './Header.css'
 
 function Header(props) {
     return (
@@ -51,6 +50,7 @@ function Header(props) {
                         </Navbar.Brand>
                     </div>
                     <Navbar.Offcanvas
+                        style={{ width: '300px' }}
                         id={`offcanvasNavbar-expand-false`}
                         aria-labelledby={`offcanvasNavbarLabel-expand-false`}
                         placement="start"
@@ -60,32 +60,16 @@ function Header(props) {
                                 MENU
                             </Offcanvas.Title>
                         </Offcanvas.Header>
-                        <Offcanvas.Body>
-                            <Nav className="justify-content-end flex-grow-1 pe-3">
-                                <NavDropdown
-                                    title="Dropdown"
-                                    id={`offcanvasNavbarDropdown-expand-false`}
-                                >
-                                    <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action4">
-                                        Another action
-                                    </NavDropdown.Item>
-                                    <NavDropdown.Item href="#action5">
-                                        Something else here
-                                    </NavDropdown.Item>
-                                </NavDropdown>
-                                <Link to='/'>Home</Link>
-                                <Link to='/login'>Login</Link>
+                        <Offcanvas.Body className="m-0 p-0">
+                            <Nav className="justify-content-end flex-grow-1">
+                                <Link className='menu-item' to='/products'>CHANTELLE</Link>
+                                <Link className='menu-item' to='/products'>READY TO WEAR</Link>
+                                <Link className='menu-item' to='/products'>DUPATTAS</Link>
+                                <Link className='menu-item' to='/products'>SHAWLS</Link>
+                                <Link className='menu-item' to='/products'>UNSTITCHED</Link>
+                                <Link className='menu-item' to='/products'>BOTTOMS</Link>
                             </Nav>
-                            <Form className="d-flex">
-                                <Form.Control
-                                    type="search"
-                                    placeholder="Search"
-                                    className="me-2"
-                                    aria-label="Search"
-                                />
-                                <Button variant="outline-success">Search</Button>
-                            </Form>
+                            <div className="menu-social"></div>
                         </Offcanvas.Body>
                     </Navbar.Offcanvas>
                 </Container>
