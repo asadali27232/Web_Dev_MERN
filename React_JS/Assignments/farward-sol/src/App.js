447b2e877b6c9f5774c0819ad1fff0e752939527
Asad Ali <asadali27232@gmail.com>
2023-02-02 09:29:54 +0500

    Updated

--- React_JS/Assignments/farward-sol/src/App.js
+++ React_JS/Assignments/farward-sol/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Header from './components/Header'
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
-      <Head
+      <Header />
+      <Footer />
     </>
   );
 }
--- React_JS/Assignments/farward-sol/src/components/Footer.js
+++ React_JS/Assignments/farward-sol/src/components/Footer.js
@@ -1,40 +1,10 @@
 import React from 'react'
 import './Footer.css'
-import { Link } from 'react-router-dom'
 
 function Footer() {
     return (
         <footer>
             <div className="foot1">
-                <ul className="foot1-ul">
-                    <li><Link to="/">WHO WE ARE</Link></li>
-                    <div className="ver_bar"></div>
-                    <li><Link to="/">CONTACT US</Link></li>
-                    <div className="ver_bar"></div>
-                    <li><Link to="/">OUR RESPONSIBILITY</Link></li>
-                    <div className="ver_bar"></div>
-                    <li><Link to="/">EXCHANGE & REFUND</Link></li>
-                    <div className="ver_bar"></div>
-                    <li><Link to="/">EXCHANGE FORM</Link></li>
-                    <div className="ver_bar"></div>
-                    <li><Link to="/">SHIPPING</Link></li>
-                    <div className="ver_bar"></div>
-                    <li><Link to="/">LOOK BOOK</Link></li>
-                    <div className="ver_bar"></div>
-                    <li><Link to="/">SERVICE WE PROVIDE</Link></li>
-                    <div className="ver_bar"></div>
-                    <li><Link to="/">TRACK YOUR ORDER</Link></li>
-                    <div className="ver_bar"></div>
-                    <li><Link to="/">ORDER CANCELLATION FORM</Link></li>
-                    <div className="ver_bar"></div>
-                    <li><Link to="/">CAREERS</Link></li>
-                    <div className="ver_bar"></div>
-                    <li><Link to="/">LEGAL</Link></li>
-                    <div className="ver_bar"></div>
-                    <li><Link to="/">FAQ's</Link></li>
-                    <div className="ver_bar"></div>
-                    <li><Link to="/">PRIVACY & POLICY</Link></li>
-                </ul>
                 <div className="social_icons">
                     <ul>
                         <li>
