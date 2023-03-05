be2ce9a813fc8143da61069d630f7f5652a6826f
Asad Ali <asadali27232@gmail.com>
2023-03-05 16:51:15 +0500

    Updated

--- MERN/Assignments/invoice-client/src/CRUD.js
+++ MERN/Assignments/invoice-client/src/CRUD.js
@@ -1,67 +1,119 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './CRUD.css'
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./CRUD.css"
 
 function CRUD() {
-    const [products, setProducts] = useState([]);
-
-    useEffect(() => {
-        axios.get('http://localhost:3001/product/getProduct')
+    const [products, setProducts] = useState([])
+    const [prd_id, setPrd_id] = useState("")
+    const [hide, setHide] = useState(true)
+    const handleGet = () => {
+        axios
+            .get("http://localhost:3001/product/getProduct")
             .then((response) => {
-                setProducts(response.data);
+                setProducts(response.data)
             })
             .catch((error) => {
-                console.error('Error fetching products:', error);
-            });
-    }, []);
+                alert(`Error fetching products: ${error}`)
+            })
+    }
+    useEffect(() => {
+        handleGet()
+    }, [])
 
-    const [prdID, setPrdID] = useState(1000);
-    const [prdName, setPrdName] = useState('');
-    const [prdPrice, setPrdPrice] = useState('');
+    const [prdID, setPrdID] = useState(1001)
+    const [prdName, setPrdName] = useState("")
+    const [prdPrice, setPrdPrice] = useState("")
 
     const handleNameChange = (event) => {
-        setPrdName(event.target.value);
-    };
+        setPrdName(event.target.value)
+    }
     const handlePriceChange = (event) => {
-        setPrdPrice(event.target.value);
-    };
+        setPrdPrice(event.target.value)
+    }
 
     const handleSubmit = (event) => {
-        event.preventDefault();
-        axios.post('http://localhost:3001/product/addProduct', { prdID, prdName, prdPrice })
+        event.preventDefault()
+        if (prdName === "" || prdPrice === "") return
+        else if (prdName.indexOf(" ") <= 0 || prdName.indexOf(" ") <= 0) {
+            setPrdName("")
+            setPrdPrice("")
+            return
+        }
+        axios
+            .post("http://localhost:3001/product/addProduct", {
+                prdID,
+                prdName,
+                prdPrice,
+            })
             .then((response) => {
-                alert('Product added successfully!');
-                console.log(response)
-                setPrdName('');
-                setPrdPrice('');
+                handleGet()
+                setPrdName("")
+                setPrdPrice("")
                 setPrdID(prdID + 1)
             })
             .catch((error) => {
-                console.error('Error adding product:', error);
-            });
+                alert(`Error adding product: ${error}`)
+            })
     }
-    const editPrd = () => {
-        alert("Product edited")
+    const handleEdit = (product) => {
+        setPrd_id(product._id)
+        setPrdID(product.prdID)
+        setPrdName(product.prdName)
+        setPrdPrice(product.prdPrice)
+        setHide(false)
     }
-    const deletePrd = () => {
-        alert("Product deleted")
+    const edit = (event) => {
+        event.preventDefault()
+        axios
+            .put(`http://localhost:3001/product/updateProduct/${prd_id}`, {
+                prdID,
+                prdName,
+                prdPrice,
+            })
+            .then((response) => {
+                handleGet()
+                setTimeout(() => {
+                    alert(`Product updated successfully having ID ${prdID}`)
+                }, 100)
+                setPrdName("")
+                setPrdPrice("")
+            })
+            .catch((error) => {
+                alert(`Error adding product: ${error}`)
+            })
+        setHide(true)
+    }
+    const handleDelete = (product) => {
+        const url = `http://localhost:3001/product/deleteProduct/${product._id}`
+        axios
+            .delete(url)
+            .then((response) => {
+                handleGet()
+            })
+            .catch((error) => {
+                alert(`Error deleting product: ${error}`)
+            })
     }
     return (
         <div className="container-fluid">
             <div className="row justify-content-center align-content-center">
                 <div className="col-5 m-2 p-3 pt-0" id="prd-sec">
-                    <div className="top-title"><h4>Products</h4></div>
+                    <div className="top-title">
+                        <h4>Products</h4>
+                    </div>
                     <div className="product-container">
                         <div className="adding-prd" id="new-prd-box">
-                            <div className="adding-new-prd-top"><h4>Adding New Product</h4></div>
+                            <div className="adding-new-prd-top">
+                                <h4>Adding New Product</h4>
+                            </div>
                             <div className="new-prd-id"></div>
-                            <form onSubmit={handleSubmit}>
+                            <form>
                                 <div className="new-prd-name">
                                     <input
                                         type="text"
                                         id="add-prd-name"
-                                        name='prdName'
+                                        name="prdName"
                                         placeholder="New Product Name"
                                         value={prdName}
                                         onChange={handleNameChange}
@@ -71,7 +123,7 @@ function CRUD() {
                                     <input
                                         type="text"
                                         id="add-prd-price"
-                                        name='prdPrice'
+                                        name="prdPrice"
                                         placeholder="New Product Price"
                                         value={prdPrice}
                                         onChange={handlePriceChange}
@@ -80,10 +132,21 @@ function CRUD() {
                                 <div className="add-prd">
                                     <button
                                         id="btn-add-new-prd"
-                                        className="btn-add-product"
-                                        type='submit'
+                                        className={
+                                            !hide ? "hide" : "btn-add-product"
+                                        }
+                                        onClick={handleSubmit}
                                     >
-                                        ADD
+                                        ADD NEW
+                                    </button>
+                                    <button
+                                        id="btn-add-new-prd"
+                                        className={
+                                            hide ? "hide" : "btn-add-product"
+                                        }
+                                        onClick={edit}
+                                    >
+                                        EDIT
                                     </button>
                                 </div>
                             </form>
@@ -92,13 +155,22 @@ function CRUD() {
                 </div>
                 <div className="col-6 m-2 p-3 pt-0" id="prd-sec">
                     <div className="product-container">
-
                         <div className="prd-header">
-                            <div className="prd-id"><p>ID</p></div>
-                            <div className="prd-name"><p>NAME</p></div>
-                            <div className="prd-price"><p>PRICE</p></div>
-                            <div className="prd-buy"><p>EDIT</p></div>
-                            <div className="prd-buy"><p>DELETE</p></div>
+                            <div className="prd-id">
+                                <p>ID</p>
+                            </div>
+                            <div className="prd-name">
+                                <p>NAME</p>
+                            </div>
+                            <div className="prd-price">
+                                <p>PRICE</p>
+                            </div>
+                            <div className="prd-buy">
+                                <p>EDIT</p>
+                            </div>
+                            <div className="prd-buy">
+                                <p>DELETE</p>
+                            </div>
                         </div>
                         <div className="prd-box">
                             {products.map((product) => (
@@ -114,7 +186,7 @@ function CRUD() {
                                     </div>
                                     <div
                                         className="prd-buy"
-                                        onClick={editPrd}
+                                        onClick={() => handleEdit(product)}
                                     >
                                         <img
                                             id="0"
@@ -124,7 +196,7 @@ function CRUD() {
                                     </div>
                                     <div
                                         className="prd-buy"
-                                        onClick={deletePrd}
+                                        onClick={() => handleDelete(product)}
                                     >
                                         <img
                                             id="0"
--- MERN/Assignments/invoice-server/App/APIs/Models/ProductsModel.js
+++ MERN/Assignments/invoice-server/App/APIs/Models/ProductsModel.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 mongoose.pluralize(null)
 
 let Schema = mongoose.Schema
