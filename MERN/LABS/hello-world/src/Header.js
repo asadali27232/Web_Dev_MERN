371b1cdf6895fd3b225ce945e564a8a457b3793e
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2023-03-30 06:03:34 +0500

    Updated

--- MERN/LABS/hello-world/src/Header.js
+++
@@ -1,7 +0,0 @@
-function Header() {
-    return (
-        <>Header</>
-    )
-}
-
-export default Header()
--- MERN/LABS/hello-world/src/Love.js
+++
@@ -1,7 +0,0 @@
-function Love() {
-    return (
-        <h1>I Love You Sooo Much Jan e Yara</h1>
-    )
-}
-
-export default Love()
